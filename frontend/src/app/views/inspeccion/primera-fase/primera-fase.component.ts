b1be7c6fd7f515fe4057eb03e90c9c2516329a4d
grobles10 <147532688+grobles10@users.noreply.github.com>
2024-05-28 20:29:39 +0200

    Update primera-fase.component.ts

--- frontend/src/app/views/inspeccion/primera-fase/primera-fase.component.ts
+++ frontend/src/app/views/inspeccion/primera-fase/primera-fase.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { VehicleService } from '../../../vehicle.service';
-import { Vehicle } from '../../../services/vehicle.interface';
+import { VehicleService } from '../../../services/vehicle.service';
+import { PrimerafaseService } from '../../../services/primerafase.service';
+import { Vehicle } from '../../../interfaces/vehicle.interface';
 import {
   RowComponent,
   ColComponent,
@@ -11,6 +12,7 @@ import {
   CardBodyComponent,
   ButtonDirective
 } from '@coreui/angular';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-primera-fase',
@@ -20,6 +22,7 @@ import {
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RowComponent,
     ColComponent,
     CardComponent,
@@ -30,26 +33,34 @@ import {
 })
 export class PrimeraFaseComponent implements OnInit {
   vehiculoForm!: FormGroup;
+  vehicleId: number | null = null;
   grupos = [
     { id: 1, nombre: 'Grupo 1: Identificación', subgrupos: ['1.1 Documentación', '1.2 Número de Bastidor', '1.3 Placa de Matrícula'] },
-    { id: 2, nombre: 'Grupo 2: Acondicionamiento exterior, carrocería y chasis', subgrupos: ['2.1 Antiempotramiento delantero',
-     '2.2 Carrocería y chasis', '2.3 Dispositivos de acoplamiento', '2.4 Guardabarros y dispositivos antiproyección', '2.5 Limpia y lava parabrisas',
-    '2.6 Protecciones laterales', '2.7 Protección trasera', '2.8 Puertas y peldaños', '2.9 Retrovisores', '2.10 Señales en los vehículos',
-    '2.11 Soporte exterior de la rueda de repuesto', '2.12 Vidrios de seguridad'] },
-    { id: 3, nombre: 'Grupo 3: Acondicionamiento Interior', subgrupos: ['3.1 Asientos y sus anclajes', '3.2 Cinturones de seguridad', 
-    '3.3 Dispositivo de retención para niños','3.4 Antihielo y antivaho', '3.5 Antirrobo y alarma','3.6 Campo de visión directa',
-    '3.7 Dispositivos de retención de carga', '3.8 Indicador de velocidad y cuentakilómetros','3.9 Salientes interiores'] },
-    { id: 4, nombre: 'Grupo 4: Alumbrado y señalización', subgrupos: ['4.1 Luces de cruce y carretera', '4.2 Luz de marcha atrás', 
-    '4.3 Luces indicadoras de dirección','4.4 Señal de emergencia', '4.5 Luces de frenado','4.6 Luz de placa de matrícula trasera',
-    '4.7 Luces de posición', '4.8 Luces antiniebla','4.9 Luz de gálibo', '4.10 Catadióptricos', '4.11 Alumbrado interior',
-    '4.12 Avisador acústico', '4.13 Luz de estacionamiento', '4.14 Señalización de apertura de puertas','4.15 Señalización luminosa específica',
-    '4.16 Luces de circulación diurna'] }
+    {
+      id: 2, nombre: 'Grupo 2: Acondicionamiento exterior, carrocería y chasis', subgrupos: ['2.1 Antiempotramiento delantero',
+        '2.2 Carrocería y chasis', '2.3 Dispositivos de acoplamiento', '2.4 Guardabarros y dispositivos antiproyección', '2.5 Limpia y lava parabrisas',
+        '2.6 Protecciones laterales', '2.7 Protección trasera', '2.8 Puertas y peldaños', '2.9 Retrovisores', '2.10 Señales en los vehículos',
+        '2.11 Soporte exterior de la rueda de repuesto', '2.12 Vidrios de seguridad']
+    },
+    {
+      id: 3, nombre: 'Grupo 3: Acondicionamiento Interior', subgrupos: ['3.1 Asientos y sus anclajes', '3.2 Cinturones de seguridad',
+        '3.3 Dispositivo de retención para niños', '3.4 Antihielo y antivaho', '3.5 Antirrobo y alarma', '3.6 Campo de visión directa',
+        '3.7 Dispositivos de retención de carga', '3.8 Indicador de velocidad y cuentakilómetros', '3.9 Salientes interiores']
+    },
+    {
+      id: 4, nombre: 'Grupo 4: Alumbrado y señalización', subgrupos: ['4.1 Luces de cruce y carretera', '4.2 Luz de marcha atrás',
+        '4.3 Luces indicadoras de dirección', '4.4 Señal de emergencia', '4.5 Luces de frenado', '4.6 Luz de placa de matrícula trasera',
+        '4.7 Luces de posición', '4.8 Luces antiniebla', '4.9 Luz de gálibo', '4.10 Catadióptricos', '4.11 Alumbrado interior',
+        '4.12 Avisador acústico', '4.13 Luz de estacionamiento', '4.14 Señalización de apertura de puertas', '4.15 Señalización luminosa específica',
+        '4.16 Luces de circulación diurna']
+    }
   ];
+subgruposControls: any;
 
   constructor(
     private fb: FormBuilder,
-    private vehicleService: VehicleService
-  ) { }
+    private vehicleService: VehicleService,
+    private primeraFaseService: PrimerafaseService  ) { }
 
   ngOnInit(): void {
     this.vehiculoForm = this.fb.group({
@@ -100,15 +111,45 @@ export class PrimeraFaseComponent implements OnInit {
       });
     }
   }
-  
 
   get subgrupos(): FormArray {
     return this.vehiculoForm.get('subgrupos') as FormArray;
-  }  
+  }
 
   onSubmit(): void {
-    if (this.vehiculoForm.valid) {
-      console.log(this.vehiculoForm.value);
+    if (this.vehiculoForm.invalid) {
+      console.log('Formulario no válido.');
+      return;
     }
+  
+    const formValue = this.vehiculoForm.value;
+  
+    const payload = {
+      numerobastidor: formValue.numerobastidor,
+      km: formValue.kilometraje,
+      subgrupos: {
+        grupo: this.getNombreGrupoPorId(parseInt(this.vehiculoForm.get('grupoInspeccion')?.value)),
+        subgrupos: formValue.subgrupos.map((sg: any) => ({
+          nombre: sg.nombre,
+          defecto: sg.defecto
+        }))
+      }
+    };
+
+    console.log('Payload:', payload);
+  
+    this.primeraFaseService.createPrimerafase(payload).subscribe(
+      (response: any) => {
+        console.log('Datos guardados:', response);
+      },
+      (error: any) => {
+        console.error('Error guardando los datos:', error);
+      }
+    );
+  }
+
+  getNombreGrupoPorId(id: number): string {
+    const grupo = this.grupos.find(grupo => grupo.id === id);
+    return grupo ? grupo.nombre : '';
   }
 }
